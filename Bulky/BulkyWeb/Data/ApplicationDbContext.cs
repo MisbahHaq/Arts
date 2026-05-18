bd87cdac87c8affd71770819580ff3bb28d70e9c
Misbah Haque <misbahmuneer2003@gmail.com>
2026-05-18 16:19:43 +0500

    Update

--- Bulky/BulkyWeb/Data/ApplicationDbContext.cs
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,15 @@ namespace BulkyWeb.Data
             
         }
         public DbSet<Category> Categories{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, nameof ="Action", DisplayOrder = 1},
+                new Category { Id = 2, nameof = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, nameof = "History", DisplayOrder = 3 },
+
+                )
+        }
     }
 }
